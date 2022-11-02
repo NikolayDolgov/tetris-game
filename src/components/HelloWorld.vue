0ae4d9b9862025f4c9e7e60d012dd5afc1e17d48
Nikolay Dolgov <koly201020@yandex.ru>
2022-11-03 00:33:04 +0300

    Create a project on Vue.js

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import "./HelloWorld.css";
+import { matrix } from '../assets/utils/utils';
+console.log(matrix);
+
+
+</script>
+
+
+<template>
+  <div class="firstClass">
+    <div v-for="item in matrix" class="twoClass">
+      <div v-for="itemTwo in item" class="cell">{{ itemTwo }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
