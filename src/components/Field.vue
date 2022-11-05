f0b24a17bf2c0e5c27c7a154fe471d5236a4484d
Nikolay Dolgov <koly201020@yandex.ru>
2022-11-05 03:41:46 +0300

    Add a space check

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,63 +1,133 @@
 <script>
 import "./Field.css"
 import Buttonk from './button/Button.vue'
+import "./button/Button.css"
+
+import { matrix as constMatrix } from "../assets/utils/utils";
 
 export default {
   components: {
     Buttonk,
   },
-  props: {
-    matrix: Object,
-  },
-  methods: {
-    methodThatForcesUpdate() {
-      // ...
-      console.log('dd');
-      this.$forceUpdate();  // Заметьте, что мы используем $ в качестве префикса
-      // ...
+  computed: {
+    count() {
+      return this.$store.state.count
+    },
+    matrix() {
+      return this.$store.state.matrixField
     }
   },
-  data(props) {
-    const randomCFirstCell = Math.floor(Math.random() * 10);
-    let indexUpdate = 0;
-    console.log(props.matrix);
-    function сalculateMovement(matrix, indexUpdate) {
-      let found = 0;
-      for (let i in matrix) {
-        for (let j in matrix[i]) {
-          console.log("Не найден чёрный квадрат");
-          if (matrix[i][j]["status"] === 1) {
-            console.log("Найден чёрный квадрат");
-            // смещаем чёрный квадрат вниз
-            matrix[i][j]["status"] = 0;
-            console.log(String(Number(i)))
-            matrix[String(Number(i) + 1)][j]["status"] = 1;
-            matrix[i][j]["id"] = String(String(matrix[i][j]["id"]) + "new");
-            found = 1;
-            return indexUpdate += 1;
-            break
+  data() {
+    let end = false;
+    let workMatrix;
+    if (this.matrix) {
+      workMatrix = this.matrix;
+    }
+    else {
+      workMatrix = constMatrix;
+    }
+
+    const сalculateMovement = () => { // поиск квадрата с индексом 2, как только квадрат отпускается он равен 1
+      let k = 0;
+      for (let i in workMatrix) {
+        for (let j in workMatrix[i]) {
+          console.log("Не найден");
+          if (workMatrix[i][j]["status"] === 2) { // поиск квадрата на строке
+            if (i === '9') {
+              console.log('Достигли низа');
+              workMatrix[i][j]["status"] = 1;
+              // проверяем заполнена ли девятая линия полностью.
+              let check = 0;
+              for (let y in workMatrix[i]) {
+                if (workMatrix[i][y] === 0) {
+                  break;
+                }
+                else {
+                  check++
+                }
+              }
+              if (check === 10) { // очищаем девятую линию
+                let newWorkMatrix = {
+                  '0': {
+                    '0': { status: 0, id: 1 },
+                    '1': { status: 0, id: 2 },
+                    '2': { status: 0, id: 3 },
+                    '3': { status: 0, id: 4 },
+                    '4': { status: 0, id: 5 },
+                    '5': { status: 0, id: 6 },
+                    '6': { status: 0, id: 7 },
+                    '7': { status: 0, id: 8 },
+                    '8': { status: 0, id: 9 },
+                    '9': { status: 0, id: 10 },
+                  },
+                };
+                let newRow = 0;
+                for (let row in workMatrix) {
+                  /*newRow++
+                  newWorkMatrix[String(newRow)] = workMatrix[row];
+                  if(newRow === 9){
+                    console.log(newWorkMatrix)
+                    console.log(workMatrix)
+                    workMatrix = newWorkMatrix
+                    break;
+                  }*/
+
+                }
+
+
+                doNewMatrix(0, Math.floor(Math.random() * 10));
+              }
+            }
+            else if (i !== '9' && workMatrix[String(Number(i) + 1)][j]["status"] === 1) { // поиск квадарта под квадартом
+              doNewMatrix(0, Math.floor(Math.random() * 10));
+              console.log('Достигли низа (квадрат)');
+              workMatrix[i][j]["status"] = 1;
+            }
+            else if (i === '0' && workMatrix[String(Number(i) + 1)][j]["status"] === 1) { // поиск квадарта под квадартом на проигрыш
+              alert('Вы проиграли');
+              end = true; // можно удалить счётчик
+              workMatrix[i][j]["status"] = 3;
+            }
+            else {
+              // смещаем чёрный квадрат вниз
+              workMatrix[i][j]["status"] = 0;
+              workMatrix[String(Number(i) + 1)][j]["status"] = 2;
+            }
+            console.log('ff')
+            k = 1;
+            //this.$store.commit('сhangeKey', workMatrix)
+            break;
           }
         }
-        if (found === 1)
+        if (k === 1) {
           break;
+        }
       }
     }
 
-    function doNewMatrix(addressGlobal, addressLocal) {
+    const doNewMatrix = (addressGlobal, addressLocal) => {
       const firstOrder = addressGlobal;
       const secondOrder = addressLocal;
-      props.matrix[firstOrder][secondOrder]["status"] += 1
-      return props.matrix;
+      console.log(workMatrix)
+      workMatrix[firstOrder][secondOrder]["status"] += 2
+      //this.$store.commit('сhangeKey', workMatrix)
     }
 
-    doNewMatrix(0, randomCFirstCell);
-    сalculateMovement(props.matrix);
+    doNewMatrix(0, Math.floor(Math.random() * 10));
+    //сalculateMovement(workMatrix);
 
     setInterval(() => {
-      indexUpdate = сalculateMovement(props.matrix, indexUpdate);
-      console.log(indexUpdate)
-      this.forceRerender()
-    }, 3000);
+      if (!end){
+        сalculateMovement();
+        //count++
+        this.$store.commit('increment')
+        //this.$store.commit('сhangeKey', workMatrix)
+      }
+        
+
+      
+      //this.$store.commit('сhangeKey', workMatrix)
+    }, 50);
 
 
     //const updateTime = () => setTimeout(сalculateMovement(props.matrix), 3000);
@@ -65,34 +135,26 @@ export default {
 
     return {
       doNewMatrix,
-      indexUpdate,
-      renderComponent: true,
-    }
-  },
-  methods: {
-    forceRerender() {
-      // Сначала скроем компонент
-      this.renderComponent = false;
-
-      this.$nextTick(() => {
-        // А потом покажем снова
-        console.log('ddddddddddddddddddddddd')
-        this.renderComponent = true;
-      });
+      workMatrix,
     }
   },
 }
+
+//<Buttonk v-for="(itemTwo, indexTwo) in item" :key=itemTwo.id :mean=itemTwo.status :addressGlobal=index :addressLocal=indexTwo
+//@newMatrix=doNewMatrix />
 </script>
 
 <template>
-  <div class="firstClass">
-    <div v-for="(item, index) in matrix" class="twoClass">
-      <Buttonk v-for="(itemTwo, indexTwo) in item" 
-        :key=itemTwo.id 
-        :mean=itemTwo.status 
-        :addressGlobal=index
-        :addressLocal=indexTwo 
-        @newMatrix=doNewMatrix />
+  <div>
+    <p>{{ this.count }}</p>
+    <div class="firstClass">
+      <div v-for="(item, index) in this.matrix" class="twoClass">
+        <div v-for="(itemTwo, indexTwo) in item" class="threeClass">
+          <div v-if="itemTwo.status >= 3" class="cell cell__color_red">{{ itemTwo.status }}</div>
+          <div v-if="itemTwo.status >= 1" class="cell cell__color_blue">{{ itemTwo.status }}</div>
+          <div v-else class="cell">{{ itemTwo.status }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,20 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import { matrix } from '../assets/utils/utils';
 
 const store = createStore({
   state () {
     return {
-      count: 0
+      count: 0,
+      matrixField: matrix,
     }
   },
+
   mutations: {
     increment (state) {
       state.count++
+    },
+    сhangeKey (state, newMatrix) {
+      state.matrixField = newMatrix
     }
   }
 })
